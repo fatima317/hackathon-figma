25d572ddc6ea05314b8760b1a4bf753147e1413b
fatima_97 <160069921+fatima317@users.noreply.github.com>
2024-12-09 21:27:05 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Footer from "@/components/ui/footer";
 import Hero from "@/components/ui/hero";
+import LatestBlog from "@/components/ui/latestBlog";
+import MiddleOne from "@/components/ui/middleOne";
 
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <MiddleOne />
+      <LatestBlog />
       <Footer />
     </div>
   );
