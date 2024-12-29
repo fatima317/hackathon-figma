fe8a94fbcb6505ab2107cfb64a013b87f953ad42
fatima_97 <160069921+fatima317@users.noreply.github.com>
2024-12-29 05:06:45 +0500

    main

--- app/singleBlog/page.tsx
+++ app/singleBlog/page.tsx
@@ -32,7 +32,7 @@ const SingleBlog = () => {
       {/* Main Content Section */}
       <div className='flex flex-wrap pt-20 pb-10 px-4 md:px-20 md:gap-6'>
          {/* Left Content */}
-        <div className='w-full md:max-w-[870px] md:w-[70%]'>
+        <div className='w-full md:max-w-[870px] md:w-[68%]'>
         <div className='flex flex-col md:px-4'>
           {/* Main Blog Image */}
           <Image 
