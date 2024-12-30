c1904176bfefd4570b26852ac0b6d2dfd2625f8a
fatima_97 <160069921+fatima317@users.noreply.github.com>
2024-12-31 02:17:09 +0500

    main

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -10,8 +10,8 @@ const Hero = () => {
           <Image
             src="/lamp.png"
             alt="Lamp"
-            width={257}
-            height={257}
+            width={187}
+            height={187}
             objectFit="cover"
             className="object-contain"
           />
