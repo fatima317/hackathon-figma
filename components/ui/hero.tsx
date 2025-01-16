368283f55c49867dffe63bb5e29e54f23c13c4ae
fatima_97 <160069921+fatima317@users.noreply.github.com>
2025-01-17 02:42:47 +0500

    main

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
       </div>
 
       {/* Center Column: Content (Title, Description, Button) */}
-      <div className="md:w-[44%] md:max-w-[644px] text-center flex flex-col md:items-start items-center justify-center py-6">
+      <div className="md:w-[44%] md:max-w-[644px] text-center md:text-left flex flex-col justify-center py-6">
         <p className="text-[16px] text-pink-500 font-medium">
           Best Furniture For Your Castle...
         </p>
